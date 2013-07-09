a0cb6c0b353d38d7103d3edee68d1ad73f1d732d
Mads Bielefeldt <mads.bielefeldt@gmail.com>
2013-07-09 17:35:05 +0200

    Tap animation added

--- KitchenSink/KitchenSinkViewController.m
+++ KitchenSink/KitchenSinkViewController.m
@@ -11,9 +11,44 @@
 
 @interface KitchenSinkViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *kitchenSink;
+
 @end
 
 @implementation KitchenSinkViewController
+
+#define MOVE_DURATION 3.0
+
+- (IBAction)tap:(UITapGestureRecognizer *)sender
+{
+    CGPoint tapLocation = [sender locationInView:self.kitchenSink];
+    
+    for (UIView *view in self.kitchenSink.subviews) {
+        if (CGRectContainsPoint(view.frame, tapLocation)) {
+            [UIView animateWithDuration:MOVE_DURATION animations:^{
+                [self setRandomLocationForView:view];
+            }];        }
+    }
+}
+
+- (void)addFood:(NSString *)food
+{
+    UILabel *foodLabel = [[UILabel alloc] init];
+    foodLabel.text = food;
+    foodLabel.font = [UIFont systemFontOfSize:46];
+    foodLabel.backgroundColor = [UIColor clearColor];
+    [self setRandomLocationForView:foodLabel];
+    [self.kitchenSink addSubview:foodLabel];
+}
+
+- (void)setRandomLocationForView:(UIView *)view
+{
+    [view sizeToFit];
+    CGRect sinkBounds = CGRectInset(self.kitchenSink.bounds, view.frame.size.width / 2, view.frame.size.height / 2);
+    CGFloat x = arc4random() % (int)sinkBounds.size.width + view.frame.size.width / 2;
+    CGFloat y = arc4random() % (int)sinkBounds.size.height + view.frame.size.height / 2;
+    view.center = CGPointMake(x, y);
+}
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
@@ -30,7 +65,7 @@
 - (IBAction)doneAsking:(UIStoryboardSegue *)segue
 {
     AskerViewController *askerVC = segue.sourceViewController;
-    NSLog(@"%@", askerVC.answer);
+    [self addFood:askerVC.answer];
 }
 
 @end
